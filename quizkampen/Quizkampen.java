52b4db27b4ac39d1bdaf9da09296b05bf6db983a
Marcus Damsgaard Jensen <marcus8209@gmail.com>
2018-11-14 14:24:42 +0100

    testing commit marcus dj2fd

--- quizkampen/Quizkampen.java
+++ quizkampen/Quizkampen.java
@@ -13,7 +13,8 @@ import javafx.stage.Stage;
  * @author 224Mi
  */
 public class Quizkampen extends Application {
-    // testing testing marcus dj 2
+    // testing testing marcus dj 3
+    
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
