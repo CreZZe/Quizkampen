21226a5bc3cb8356040834b23cee4044bc4dae2c
nikalsh <Niklas.Helander@yh.nackademin.se>
2018-11-14 14:09:12 +0100

    123abc

--- quizkampen/Quizkampen.java
+++ quizkampen/Quizkampen.java
@@ -19,7 +19,7 @@ public class Quizkampen extends Application {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+            asdasdasdaszcv
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
