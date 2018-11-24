a00aa13311a466283446a27bab32ab076130f182
nikalsh <niklas@helander.se>
2018-11-24 01:30:51 +0100

    forgot to add

---
+++ server/Game.java
@@ -0,0 +1,25 @@
+package server;
+
+/**
+ *
+ * @author nikalsh
+ */
+public class Game extends Thread {
+
+    private int rounds;
+    private int currentRound;
+    private ClientHandler[] players;
+    private quizkampen.QuestionObject[] category;
+
+    public Game() {
+
+        //prepare to read from files and generate questions objects
+    }
+
+    @Override
+    public void run() {
+
+        //do the game
+    }
+
+}
