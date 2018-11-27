c697b5ae563f79b01b26a21475224175a33c42d1
Marcus Damsgaard Jensen <marcus8209@gmail.com>
2018-11-27 17:41:56 +0100

    laggt till RoundScreen och Gamescreen i GUIt

--- quizkampen/Lobbyscreen.java
+++ quizkampen/Lobbyscreen.java
@@ -69,10 +69,10 @@ public class Lobbyscreen {
         });
 
         newGameButton.setOnAction(e -> {
-            System.out.println(Quizkampen.client.sendRequestAndGetResponse("newGame"));
-            Scene questionScene = new Scene(new Questionscreen(window, startScene, windowWidth, windowHeight).getGUI(), windowWidth, windowHeight);
-            questionScene.getStylesheets().add("Styling.css");
-            window.setScene(questionScene);
+            //System.out.println(Quizkampen.client.sendRequestAndGetResponse("newGame"));
+            Scene gameScene = new Scene(new Gamescreen(window, startScene, windowWidth, windowHeight).getGUI(), windowWidth, windowHeight);
+            gameScene.getStylesheets().add("Styling.css");
+            window.setScene(gameScene);
             
             
         });
